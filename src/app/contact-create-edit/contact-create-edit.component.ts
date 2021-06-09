34d125005c5600b9cb5bcb601e280787bd9625d5
Leonardo Lopes da Luz <leonardolluzti@gmail.com>
2021-06-09 01:37:56 -0300

    Contato Atualizado

--- src/app/contact-create-edit/contact-create-edit.component.ts
+++ src/app/contact-create-edit/contact-create-edit.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ContactService } from '../service/contact/contact.service';
+import Swal from 'sweetalert2';
+import { Contact } from '../model';
 
 @Component({
   selector: 'app-contact-create-edit',
@@ -6,13 +11,90 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-create-edit.component.scss']
 })
 export class ContactCreateEditComponent implements OnInit {
+  contact: Contact = null;
+  contactForm = new FormGroup({
+    id: new FormControl(''),
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl('', [Validators.required])
+  });
 
-  constructor() { }
+  constructor(private router: Router, public contactService: ContactService) { }
 
   ngOnInit(): void {
+    this.contactService.botaoEdit.subscribe( edit => {
+      this.contact = edit;
+      this.contactForm.get('id').setValue(edit.id);
+      this.contactForm.get('name').setValue(edit.name);
+      this.contactForm.get('email').setValue(edit.email);
+      this.contactForm.get('phone').setValue(edit.phone);
+    });
   }
-  criaContato(): void {    
-    
+
+  createContact(){
+    if (this.contactForm.valid){
+      this.contact = this.contactForm.value;
+      this.contactService.createContact(this.contact).subscribe(
+        data => {
+          Swal.fire({
+            title: 'Sucesso!',
+            text: 'Contato criado com sucesso',
+            icon: 'success',
+            confirmButtonText: 'Ok'
+          });
+          this.router.navigate(['/contact_list']);
+        },
+        error => {
+          Swal.fire({
+            title: 'Erro!',
+            text: 'Erro ao criar contato',
+            icon: 'error',
+            confirmButtonText: 'Ok'
+          });
+        }
+      );
+    }else{
+      Swal.fire({
+        title: 'Erro!',
+        text: 'Preencha todos os campos',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      });
+    }
   }
- 
+
+
+  updateContact(){
+    if (this.contactForm.valid){
+      this.contact = this.contactForm.value;
+      this.contactService.updateContact(this.contact).subscribe(
+        data => {
+          Swal.fire({
+            title: 'Sucesso!',
+            text: 'Contato editado com sucesso',
+            icon: 'success',
+            confirmButtonText: 'Ok'
+          });
+          this.router.navigate(['/contact_list']);
+        },
+        error => {
+          Swal.fire({
+            title: 'Erro!',
+            text: 'Erro ao editar contato',
+            icon: 'error',
+            confirmButtonText: 'Ok'
+          });
+        }
+      );
+    }else{
+      Swal.fire({
+        title: 'Erro!',
+        text: 'Preencha todos os campos',
+        icon: 'error',
+        confirmButtonText: 'Ok'
+      });
+    }
+  }
+
+
 }
--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from '../model';
+import { ContactService } from '../service/contact/contact.service';
+import Swal from 'sweetalert2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-contact-list',
@@ -6,10 +10,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-list.component.scss']
 })
 export class ContactListComponent implements OnInit {
-
-  constructor() { }
+ contactList: Contact[] = [];
+  constructor(private contactService: ContactService, private router: Router) {
+  }
 
   ngOnInit(): void {
+    this.getContacts();
   }
-  
+
+  getContacts(){
+    this.contactService.getContacts().subscribe(
+      data => {
+        this.contactList = data;
+      },
+      error => {
+        Swal.fire({
+          title: 'Erro!',
+          text: 'Erro ao retornar lista',
+          icon: 'error',
+          confirmButtonText: 'Ok'
+        });
+      }
+    );
+  }
+
+  deleteContact(id: number){
+    this.contactService.deleteContacts(id).subscribe(
+      data => {
+        Swal.fire({
+          title: 'Sucesso!',
+          text: 'Sucesso ao remover contato',
+          icon: 'success',
+          confirmButtonText: 'Ok'
+        });
+        this.router.navigate(['/contact_list']);
+      }      
+    );
+  }
+
+  goToCreate(){
+    this.router.navigate(['/contact']);
+  }
+
+  editContact(contact: Contact){
+    this.contactService.getContactForList(contact);
+    this.router.navigate(['/contact']);
+  }
 }
